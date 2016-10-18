9cce6c738c81a2bff5345cd4cbf78944dbe79e33
Tandel <st34027n@pace.edu>
2016-10-18 16:50:03 -0400

    JSON Parse

--- pullQuery.php
+++ pullQuery.php
@@ -3,14 +3,14 @@
 //https://demo.twilio.com/welcome/sms/reply/
 /* Read the contents of the 'Body' field of the Request. */ 
 
-//$body = $_REQUEST['Body']; 
-//
-///* Remove formatting from $body until it is just lowercase characters without punctuation or spaces. */ 
-//
-//$result = preg_replace("/[^A-Za-z0-9]/u", " ", $body); 
-//$result = trim($result); 
-//$result = strtolower($result); 
+$body = $_REQUEST['Body']; 
 
-echo "<Response><Sms>Thanks for the message. Configure your number's SMS URL to change this message.Reply HELP for help.Swapnil Tandel.</Sms></Response>";
+/* Remove formatting from $body until it is just lowercase characters without punctuation or spaces. */ 
+
+$result = preg_replace("/[^A-Za-z0-9]/u", " ", $body); 
+$result = trim($result); 
+$result = strtolower($result); 
+
+echo "<Response><Sms>Thanks for the message. Configure your number's SMS URL to change this message.Reply HELP for help. Swapnil Tandel.</Sms></Response>";
 
 ?>
